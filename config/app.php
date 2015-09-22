6d44e4aaece719f0437dbdcea2d79a20ddaecd18
petrovitch <kennthompson@gmail.com>
2015-09-22 16:40:35 -0500

    four tables 186

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        'Zizaco\Entrust\EntrustServiceProvider',
 
     ],
 
@@ -196,6 +197,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
+        'Entrust' => 'Zizaco\Entrust\EntrustFacade',
     ],
 
 ];
