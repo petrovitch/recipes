e8666906cbc470e8045c001ebce1ef56fe57f0f8
petrovitch <kennthompson@gmail.com>
2015-09-23 15:35:15 -0500

    admin home page

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,6 +17,7 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="active"><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
+                <li><a href="/admin">Admin</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li><a href="/tickets">Tickets</a></li>
                 <li><a href="/admin/users">Users</a></li>
