c0037794bd702c8331c2ab4d78029e5c0ce6c190
petrovitch <kennthompson@gmail.com>
2015-09-23 15:56:52 -0500

    refactor navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,12 +17,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="active"><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
-                <li><a href="/admin">Admin</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li><a href="/tickets">Tickets</a></li>
-                <li><a href="/admin/users">Users</a></li>
-                <li><a href="/admin/roles">Roles</a></li>
-                <li><a href="/welcome">Welcome</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown
                         <span class="caret"></span></a>
